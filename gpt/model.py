e303ef56e74e62ad8e0c920eebc3f1eff60aeff2
James Davey <jamsdavy123@gmail.com>
2024-12-31 12:10:04 +0000

    kv caching that is actually faster than standard inference (￣o￣)

--- gpt/model.py
+++ gpt/model.py
@@ -11,6 +11,7 @@ https://github.com/huggingface/transformers/blob/main/src/transformers/models/gp
 # - MoE
 # - RoPE
 # - Hyper-connections
+# - LoRA
 # - Better init
 
 import time
@@ -57,77 +58,52 @@ class CausalSelfAttention(nn.Module):
             self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
 
-        self.c_attn_cache = None
-
-    def forward(self, x, first_pass=True):
+ 
+    def forward(self, x, kv_pair=None):
         """What to cache, that is the question.
 
         - All keys, values but not queries (Note: q @ k.T so only bottom query/query for most recent token is needed)
         - In a input x of shape (B, T, C) after the first cache we only want to calculate (B, [-1], C)
 
-        TODO: No need to cache queries. The whole caching method could be more memory efficient.
+        # 'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
         """
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        t0 = time.time()
-        # Ideally just cache kv, not q.
-        if self.c_attn_cache is None: # Awfully inefficient but demonstrates the principle
-            proj = self.c_attn(x)
-            self.c_attn_cache = proj
-        else:
-            last_layer = x[:, [-1], :]
-            last_proj = self.c_attn(last_layer)
-            proj = torch.cat([self.c_attn_cache, last_proj], dim=1).contiguous()
-            self.c_attn_cache = proj
+        B, T, C = x.size() 
 
+        proj = self.c_attn(x)
         q, k, v  = proj.split(self.n_embd, dim=2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
-        # Note (n-1, n-1) attention matrix doesn't change.
-        # n = 3: torch.Size([1, 12, 3, 3])
-        # tensor([[1.0000, 0.0000, 0.0000],
-        #         [0.4673, 0.5327, 0.0000],
-        #         [0.0990, 0.0756, 0.8254]], device='cuda:0')
+        if kv_pair is not None:
+            cached_k, cached_v = kv_pair
+            k = torch.cat([cached_k, k], dim=2).contiguous()
+            v = torch.cat([cached_v, v], dim=2).contiguous()
 
-        # n = 4: torch.Size([1, 12, 4, 4])
-        #  tensor([[1.0000, 0.0000, 0.0000, 0.0000],
-        #         [0.4673, 0.5327, 0.0000, 0.0000],
-        #         [0.0990, 0.0756, 0.8254, 0.0000],
-        #         [0.2440, 0.1306, 0.1306, 0.4948]], device='cuda:0')
-        if first_pass:
-            t0 = time.time()
-            if self.flash:
-                # efficient attention using Flash Attention CUDA kernels
-                y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-                # print(f'Flash attention time taken: {time.time() - t0} seconds')
-            else:
+        first_pass = kv_pair is None
+        if self.flash:
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=first_pass)
+        else:
+            if first_pass:
                 # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
                 att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+                T = q.size(2)
                 att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
                 att = F.softmax(att, dim=-1)
                 att = self.attn_dropout(att)
                 y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-                # print(f'None attention time taken: {time.time() - t0} seconds')
+            else:
+                att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+                att = F.softmax(att, dim=-1)
+                att = self.attn_dropout(att) # shape: torch.Size([1, 12, 1, 4])
+                y = att @ v
+           
+        T = q.size(2)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = self.resid_dropout(self.c_proj(y))
+        return y, (k, v)
 
-            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-            y = self.resid_dropout(self.c_proj(y))
-            return y
-
-        if self.flash:
-            end_y = torch.nn.functional.scaled_dot_product_attention(q[:, :, [-1], :], k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=False)
-        else:
-            att = (q[:, :, [-1], :] @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att) # shape: torch.Size([1, 12, 1, 4])
-            end_y = att @ v
-        end_y = self.resid_dropout(self.c_proj(end_y.contiguous().view(B, 1, -1)))
-        return end_y
-
-    def reset_cache(self):
-        self.c_attn_cache = None
 
 class MLP(nn.Module):
 
@@ -139,7 +115,7 @@ class MLP(nn.Module):
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
-    def forward(self, x, first_pass=True):
+    def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
@@ -157,14 +133,11 @@ class Block(nn.Module):
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
         self.mlp = MLP(config, block_num)
 
-    def forward(self, x, first_pass=True):
-        if first_pass:
-            x = x + self.attn(self.ln_1(x))
-            x = x + self.mlp(self.ln_2(x))
-        else:
-            x[:, [-1], :] = x[:, [-1], :] + self.attn(self.ln_1(x), first_pass=first_pass)
-            x[:, [-1], :] = x[:, [-1], :] + self.mlp(self.ln_2(x[:, [-1], :]), first_pass=first_pass)
-        return x
+    def forward(self, x, kv_pair=None):
+        attn_out, kv_pair = self.attn(self.ln_1(x), kv_pair)
+        x = x + attn_out
+        x = x + self.mlp(self.ln_2(x))
+        return x, kv_pair
 
 
 @dataclass
@@ -209,9 +182,6 @@ class GPT(nn.Module):
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
 
-    def reset_cache(self):
-        for block in self.transformer.h:
-            block.attn.reset_cache()
 
     def get_num_params(self, non_embedding=True):
         """
@@ -233,7 +203,7 @@ class GPT(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx, targets=None, first_pass=True):
+    def forward(self, idx, targets=None, kv_pairs=None):
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
@@ -244,8 +214,17 @@ class GPT(nn.Module):
         tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
         pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
         x = self.transformer.drop(tok_emb + pos_emb)
-        for block in self.transformer.h:
-            x = block(x, first_pass=first_pass)
+        T = x.size(1)
+
+        new_kv_pairs = []
+        for block_num in range(self.config.n_layer):
+            block = self.transformer.h[block_num]
+            if kv_pairs is None:
+                x, new_kv_pair = block(x)
+            else:
+                x = x[:, [-1], :]
+                x, new_kv_pair = block(x, kv_pairs[block_num])
+            new_kv_pairs.append(new_kv_pair)
         x = self.transformer.ln_f(x)
 
         if targets is not None:
@@ -257,7 +236,7 @@ class GPT(nn.Module):
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
             loss = None
 
-        return logits, loss
+        return logits, loss, new_kv_pairs
 
     def crop_block_size(self, block_size):
         # model surgery to decrease the block size if necessary
@@ -377,13 +356,13 @@ class GPT(nn.Module):
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
 
-        self.reset_cache()
+        kv_pairs = None
         for token_num in range(max_new_tokens):
             # print(f'--- New token ({token_num}) ---')
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
             # forward the model to get the logits for the index in the sequence
-            logits, _ = self(idx_cond, first_pass=token_num == 0)
+            logits, _, kv_pairs = self(idx_cond, kv_pairs=kv_pairs)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
